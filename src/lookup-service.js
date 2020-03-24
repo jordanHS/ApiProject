eb95e59ca3f1c47102e0910d6aaeb0b0feab8680
Jordan Safford <jordansafford@gmail.com>
2020-03-24 10:14:29 -0700

    build look-up service file

---
+++ src/lookup-service.js
@@ -0,0 +1,11 @@
+export class LookupService {
+    async findDoctors(firstName, lastName) {
+        try{
+            let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+            let jsonifiedResponse = await response.json();
+            return jsonifiedResponse;
+        } catch(error) {
+            return response.status(400).send(error);
+        }
+    }
+}
