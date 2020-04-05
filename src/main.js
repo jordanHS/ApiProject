9ed4df72f348996da7477d0e2d72121a9f0574ba
Jordan Safford <jordansafford@gmail.com>
2020-04-05 14:27:21 -0700

    refactor & fix errors. allow JSON to displayn in DOM

--- src/main.js
+++ src/main.js
@@ -7,35 +7,23 @@ import { PokemonSearch } from './lookup-service';
 $(document).ready(function() {
     $('#findPokemon').click(function() {
         const type = $("#type").val();
-            $('#type').val("");
+        $('#type').val("");
 
-        const findPokemon = new PokemonSearch()
+        const search = new PokemonSearch();
         
+    search.findPokemon(type).then((response) => {
+      $("#showPokemon").empty();
+      const pokemonList = response.pokemon;
 
-
-
-        findPokemon.PokemonSearch(type)
-            .then((response) => {
-                $("#showPokemon").empty();
-                const pokemonList = response;
-
-                for(let i in pokemonList) {
-                    if( pokemonList[i] > 0) {
-                        $("#showPokemon").append(`${pokemonList[i].pokemon}`)
-                    } else if(pokemonList[i].length == 0)
-                    $('.showPokemon').text(`No Pokemon of that type were found.`);
-                }
-                getElements(response); 
-            })
-
-        const getElements = function(response) {
-             if(response.meta.error){
-               if(response.meta.http_status_code == 400)
-                   $('.showPokemon').text(`Please enter a type`);
-              return;
-            }
+      for (let poke of pokemonList) {
+        if  (pokemonList.length > 0) {
+            $(".showPokemon").append(`${poke.pokemon.name} `);
+        }  else if(pokemonList.length == 0) {
+            $('.showPokemon').text(`No Pokemon of that type were found.`);
           }
-          });
-        });
+        }
+    });
+  });
+});
 
         
