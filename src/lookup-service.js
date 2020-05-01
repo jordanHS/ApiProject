29715914f82917a9765ac00c7860d543e1623e3a
Jordan Safford <jordansafford@gmail.com>
2020-04-30 19:55:56 -0700

    fix API key

--- src/lookup-service.js
+++ src/lookup-service.js
@@ -1,6 +1,6 @@
 export class PokemonSearch {
     async findPokemon(type) {
-        let response = await fetch(`ç`);
+        let response = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
         let jsonifiedResponse = await response.json();
         return jsonifiedResponse;
         }
